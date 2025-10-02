3b037617b22d155bcd1e026313073af65773105e
kd25pm <kunal@pingmedia.org>
2025-10-02 13:15:12 +0530

    chore:cookie enable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
           {children}
 
           <Footer />
-          {/* <CookieConsent /> */}
+          <CookieConsent />
 
           <ContactFloating />
         </PinGate>
